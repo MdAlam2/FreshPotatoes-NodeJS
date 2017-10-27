2495f70e70f7e151700128fca070810cc29b9602
Mike Dang <mike@packstack.co>
2017-10-27 10:45:11 -0400

    Connected to Sqlite

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const sqlite = require('sqlite'),
       Sequelize = require('sequelize'),
       request = require('request'),
+      dotenv = require('dotenv'),
       express = require('express'),
       app = express();
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+require('dotenv').config();
+
+const {
+  PORT=3000,
+  NODE_ENV='development',
+  DB_PATH='./db/database.db'
+} = process.env;
+
 const API_BASE_URL = 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1';
+const ERR_BAD_REQUEST = '"message" key missing';
+
+// const sequelize = new Sequelize(`${ process.env.DB_DIALECT }://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }/${ process.env.DB_NAME }`);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  storage: process.env.DB_PATH
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection established successfully');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // START SERVER
 Promise.resolve()
@@ -17,7 +41,7 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 app.get('*', function(req, res){
   res.status(404).json({
-    message: '"message" key missing'
+    message: ERR_BAD_REQUEST
   })
 });
 
@@ -27,14 +51,14 @@ function getFilmRecommendations(req, res) {
 
   if (!Number.isInteger(parseInt(req.params.id, 10))) {
     res.status(422).json({
-      message: '"message" key missing'
+      message: ERR_BAD_REQUEST
     });
   }
 
   if (req.query.limit) {
     if (!Number.isInteger(parseInt(req.query.limit, 10))) {
       res.status(422).json({
-        message: '"message" key missing'
+        message: ERR_BAD_REQUEST
       });
     }
 
@@ -44,7 +68,7 @@ function getFilmRecommendations(req, res) {
   if (req.query.offset) {
     if (!Number.isInteger(parseInt(req.query.offset, 10))) {
       res.status(422).json({
-        message: '"message" key missing'
+        message: ERR_BAD_REQUEST
       });
     }
 
